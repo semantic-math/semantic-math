da3c7e45ccf3565d2ee3973b36cb1ceb94bc695e
Kevin Barabash <kevinbarabash@khanacademy.org>
2018-05-22 00:23:09 -0400

    generate es6 modules and update demo to use them

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,7 @@
+import * as Node from "../lib/es6_global/src/Node.bs.js";
+import * as Lexer from "../lib/es6_global/src/Lexer.bs.js";
+import * as MathParser from "../lib/es6_global/src/MathParser.bs.js";
+
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
@@ -5,14 +9,14 @@ const math = "f(x) = 2x + 5";
 input.value = math;
 
 const tokens = Lexer.lex(math);
-const ast = Parser.nodeToJson(Parser.parse(tokens));
+const ast = Node.toJson(MathParser.parse(tokens));
 
 output.innerText = JSON.stringify(ast, null, 2);
 
 input.addEventListener("input", () => {
     try {
         const tokens = Lexer.lex(input.value);
-        const ast = Parser.nodeToJson(Parser.parse(tokens));
+        const ast = Node.toJson(MathParser.parse(tokens));
         output.innerText = JSON.stringify(ast, null, 2);
     } catch (e) {
         output.innerText = e;
